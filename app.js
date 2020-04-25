4eb3b80b8857eb3c91dfa850e6a250e8ef8998c7
joonas175 <joonas.salojarvi@tuni.fi>
2020-04-25 18:25:30 +0300

    Serve static files, use /api/ path for api calls

--- app.js
+++ app.js
@@ -9,19 +9,26 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/ck', express.static('ckweb/dist/ckweb/'))
+
+
+/**
+ * Initialize controllers here
+ */
 var normalizedPath = path.join(__dirname, "controller");
 
 fs.readdirSync(normalizedPath).forEach((file) => {
     let controllers = require("./controller/" + file).default;
     for (let controller of controllers) {
-        console.log(controller);
+        let path = `/api${controller.path}`;
         if (controller.method === 'get') {
-            app.get(controller.path, controller.func);
+            app.get(path, controller.func);
         } else if (controller.method === 'put') {
-
+            app.put(path, controller.func);
         }
     }
 
 });
+
 
 const server = app.listen(port, (args) => console.log("Listening on port: " + port));
