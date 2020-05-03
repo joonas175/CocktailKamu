786af1a2ed81b292ae310802cf54b91dae6e9b01
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-03 17:18:49 +0300

    Lot's of stuff

--- database/BaseModel.js
+++ database/BaseModel.js
@@ -1,32 +1,125 @@
 let { dbPool } = require('./DatabaseLayer');
+const { Types } = require('./DatabaseTypes');
 
 class BaseModel {
 
-    constructor(){ }
+    constructor(){ 
+        this.columns = this.constructor.columns;
+        this.conn = this.constructor.conn;
+        this.tableName = this.constructor.tableName;
+    }
 
-    columns;
+    static get columns () {
+        throw "No columns specified!"
+    }
 
-    get conn () {
+    static get conn () {
         return dbPool.getConnection()
     }
 
-    get tableName() {
+    static get tableName() {
         throw "No table name specified"
     }
 
-    async insert() {
+    static mapToDbType(obj, columns) {
+        for(let key of Object.keys(columns)) {
+            if(obj[key] !== undefined && obj[key] !== null) {
+                switch(columns[key]){
+                    case Types.string:
+                        obj[key] = `'${obj[key]}'`; 
+                        break;
 
-        console.log()
+                    case Types.float: 
+                        if(isNaN(parseFloat(obj[key]))){
+                            throw "Number error in field " + key;
+                        } else {
+                            obj[key] = parseFloat(obj[key]); 
+                            break;
+                        }
 
-        if(this.columns === null) {
-            throw "No columns specified!"
+                    case Types.int: 
+                        if(isNaN(parseInt(obj[key]))){
+                            throw "Number error in field " + key;
+                        } else {
+                            obj[key] = parseInt(obj[key]);
+                            break;
+                        }
+                }
+            } else {
+                obj[key] = "NULL"
+            }
+        }
+        return obj;
+    }
+
+    async insert(obj) {
+
+        let conn;
+        let hasError;
+
+        try {
+
+            console.log("this columns + " + this.columns)
+
+            obj = BaseModel.mapToDbType(obj, this.columns);
+
+            let sql = `INSERT INTO ${this.tableName} `
+            + `(${Object.keys(this.columns).join(', ')})` // columns
+            + ` VALUES (${Object.keys(this.columns).map((key) => obj[key]).join(', ')});` // values
+
+            console.log(sql);
+
+            conn = await this.conn
+
+            const response = await conn.query(sql);
+
+            console.log(response);
+
+            obj.id = response.insertId;
+
+        } catch (error) {
+            console.log(error);
+            hasError = error;
         }
 
-        for(let column of Object.keys(this.columns)) {
-            console.log(column)
+        if(conn != null) conn.end();
+        
+        if(hasError !== null && hasError !== undefined) throw hasError;
+
+        return obj;
+
+    }
+
+    static async get(params) {
+
+        let hasError;
+        let resp;
+        let conn;
+
+        try {
+
+            let sql = `SELECT ${Object.keys(this.columns).join(', ')} FROM ${this.tableName} `
+            + ((params && params.where) ? params.where : '') // where clause
+            + ((params && params.order) ? params.order : '') // order clause
+
+            console.log(sql)
+            
+            conn = await this.conn
+
+            resp = await conn.query(sql);
+
+            console.log(resp)
+
+        } catch (error) {
+            console.log(error)
+            hasError = error;
         }
 
-        let conn = await this.conn
+        if(conn != null) conn.end();
+        
+        if(hasError !== null && hasError !== undefined) throw hasError;
+
+        return resp;
 
     }
 }
