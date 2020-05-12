5fc885535a62e9a7cbbe3dd90bb5a73362058f19
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-12 16:21:50 +0300

    Add Subjects

--- ckweb/src/app/user-service.service.ts
+++ ckweb/src/app/user-service.service.ts
@@ -1,40 +1,58 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Ingredient } from './entities/Ingredient';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BASE_API_URL } from './global-variables';
+import { Drink } from './entities/Drink';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  ingredients: Ingredient[] = [];
+  ingredients: BehaviorSubject<Ingredient[]> = new BehaviorSubject<Ingredient[]>([]);
 
-  availableDrinks: any[] = [];
+  availableDrinks: Subject<Drink[]> = new Subject<Drink[]>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(LOCAL_STORAGE) private storage: StorageService) {
+    this.ingredients.subscribe((value) => {
+      this.updateAvailableDrinks(value);
+    });
+
+    const ingredients = storage.get('ck-saved-ingredients');
+    if (ingredients) {
+      this.ingredients.next(ingredients);
+    }
+  }
 
   addIngredient(ingredient: Ingredient): void {
-    this.ingredients.push(ingredient);
-    this.updateAvailableDrinks();
+
+    const newIngredients = this.ingredients.getValue();
+    newIngredients.push(ingredient);
+    this.ingredients.next(newIngredients);
+    this.storage.set('ck-saved-ingredients', newIngredients);
   }
 
   removeIngredient(index: number): void {
-    this.ingredients.splice(index, 1);
-    this.updateAvailableDrinks();
+    const newIngredients = this.ingredients.getValue();
+    newIngredients.splice(index, 1);
+    this.ingredients.next(newIngredients);
+    this.storage.set('ck-saved-ingredients', newIngredients);
   }
 
-  updateAvailableDrinks(): void {
-    if(this.ingredients.length === 0) {
-      this.availableDrinks = [];
+  updateAvailableDrinks(ingredients: Ingredient[]): void {
+    if (ingredients.length === 0) {
+      this.availableDrinks.next([]);
       return;
     }
 
-    this.http.get(
+    this.http.get<Drink[]>(
       `${BASE_API_URL}/recipe`,
-      { params: new HttpParams().set('id_array', JSON.stringify(this.ingredients.map((value) => value.id)))}
+      { params: new HttpParams().set('id_array', JSON.stringify(ingredients.map((value) => value.id)))}
     ).subscribe((value) => {
-      console.log(value)
+      console.log(value);
+      this.availableDrinks.next(value);
     });
   }
 }
