82aba16560ae63f911695fb6c00d48d03e471a1d
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-12 15:22:53 +0300

    SQL magic to fetch recipes by ingredients
    Including amount of ingredients owned and total ingredients per drink

--- entities/Recipe.js
+++ entities/Recipe.js
@@ -18,8 +18,13 @@ class Recipe extends BaseModel {
     }
 
     static nativeQueryGetByRecipesByIngredientIDs(idArray) { // Longest function name ever :D
-        let sql = `SELECT * 
+        let sql = `SELECT DISTINCT id, name, description, verified, totalIngredients, ownedIngredients
         FROM recipe
+        INNER JOIN (SELECT recipe_id, COUNT(*) OVER (PARTITION BY recipe_id) AS totalIngredients FROM r_ingredient) AS temp1
+        ON temp1.recipe_id = recipe.id
+        INNER JOIN(SELECT recipe_id AS recipe_id2, COUNT(*) OVER (PARTITION BY recipe_id) AS ownedIngredients 
+            FROM r_ingredient WHERE ingredient_id IN (${idArray.join(', ')})) AS temp2
+        ON temp2.recipe_id2 = recipe.id
         WHERE id IN (SELECT DISTINCT recipe_id FROM r_ingredient WHERE ingredient_id IN (${idArray.join(', ')}))
         ;`;
 
