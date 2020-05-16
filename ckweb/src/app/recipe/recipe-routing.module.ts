0ad10242e3f6dd370956db05d6efd01d2a23f367
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-16 16:37:35 +0300

    Add page for a single recipe

--- ckweb/src/app/recipe/recipe-routing.module.ts
+++ ckweb/src/app/recipe/recipe-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { SingleRecipeComponent } from './single-recipe/single-recipe.component';
 
 
 const routes: Routes = [
   { path: 'recipe/add', component: AddRecipeComponent },
-  { path: 'recipes', component: RecipesComponent }
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipe/:id', component: SingleRecipeComponent}
 ];
 
 @NgModule({
--- ckweb/src/app/recipe/recipe.module.ts
+++ ckweb/src/app/recipe/recipe.module.ts
@@ -4,11 +4,12 @@ import { RecipeRoutingModule } from './recipe-routing.module';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SingleRecipeComponent } from './single-recipe/single-recipe.component';
 
 
 
 @NgModule({
-  declarations: [RecipesComponent],
+  declarations: [RecipesComponent, SingleRecipeComponent],
   imports: [
     CommonModule,
     RecipeRoutingModule,
