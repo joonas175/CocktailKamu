b73d3c1dd2876f3f5f228870aef6c7249dc95114
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-16 11:59:17 +0300

    Add jQuery to fix navigation on mobile devices

--- ckweb/src/app/app.component.ts
+++ ckweb/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ckweb';
+
+  ngOnInit(): void {
+
+    /**
+     * jQuery and angular together may be a bad idea but I don't care.
+     * Atleast this fixes pills not stacking up.
+     */
+    // Stack menu when collapsed
+    $('#navbarSupportedContent').on('show.bs.collapse', () => {
+      $('.nav-pills').addClass('flex-column');
+    });
+
+    // Unstack menu when not collapsed
+    $('#navbarSupportedContent').on('hide.bs.collapse', () =>  {
+      $('.nav-pills').removeClass('flex-column');
+    });
+  }
+
 }
