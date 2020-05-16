5795c9cc095d4d077b13578aca51eae0509a569e
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-16 17:46:25 +0300

    Fetch info for single drink

--- ckweb/src/app/recipe/single-recipe/single-recipe.component.ts
+++ ckweb/src/app/recipe/single-recipe/single-recipe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Drink } from 'src/app/entities/Drink';
+import { BASE_API_URL } from 'src/app/global-variables';
 
 @Component({
   selector: 'app-single-recipe',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleRecipeComponent implements OnInit {
 
-  constructor() { }
+  drink: Drink;
+
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => {
+      const id = value.get('id');
+      this.http.get<Drink>(`${BASE_API_URL}/recipe/${id}`).subscribe((drink) => {
+        this.drink = drink;
+        });
+    });
   }
 
 }
