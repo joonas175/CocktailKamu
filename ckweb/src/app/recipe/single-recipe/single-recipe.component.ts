30b001b1df1fc99b40f6b1b6492de9d2969dace1
joonas175 <joonas.salojarvi@tuni.fi>
2020-05-19 15:29:10 +0300

    Disable voting if user is not signed in

--- ckweb/src/app/recipe/single-recipe/single-recipe.component.ts
+++ ckweb/src/app/recipe/single-recipe/single-recipe.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Drink } from 'src/app/entities/Drink';
 import { BASE_API_URL } from 'src/app/global-variables';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-single-recipe',
@@ -15,9 +16,9 @@ export class SingleRecipeComponent implements OnInit {
   drink: Drink = null;
 
   currentRating = 0;
-  allowVote = true;
+  allowVote = false;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private http: HttpClient, private auth: AuthService) {
   }
 
   ngOnInit(): void {
@@ -26,12 +27,21 @@ export class SingleRecipeComponent implements OnInit {
       this.http.get<Drink>(`${BASE_API_URL}/recipe/${id}`).subscribe((drink) => {
         this.drink = drink;
         });
+
+      this.http.get<any>(`${BASE_API_URL}/vote/${id}`).subscribe((resp) => {
+        this.currentRating = resp.avgvote;
+        if (this.auth.authObj.value !== null && this.auth.authObj.value !== undefined) {
+          this.allowVote = true;
+        }
+      });
     });
   }
 
   rateChange(value: any): void {
     if (this.allowVote) {
-      console.log(value);
+      this.http.put(`${BASE_API_URL}/vote/${this.drink.id}`, null, {params: new HttpParams().set('vote', value)}).subscribe((resp) => {
+        console.log(resp);
+      });
     }
 
   }
